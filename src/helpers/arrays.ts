b3fc5bf4f7b6e3ddfd64a4db1ab0aa7e93303680
samuelhulla <oficialny.hulla@gmail.com>
2023-01-07 16:11:18 +0100

    :recycle: Cleanup: Unused code

--- src/helpers/arrays.ts
+++ src/helpers/arrays.ts
@@ -4,20 +4,3 @@
  * @returns Array flattened by one level
  */
 export const flatten = <T>(arr: T[][]): T[] => arr.reduce((acc, val) => acc.concat(val), [])
-
-/**
- * Checks whether item is identical to comparator, or matches item in comparator array
- * @param value - Item to check
- * @param against - Comparator to check against
- * @returns Boolean depending on the check
- * @example isOrIncludes('foo', 'foo') // true
- * isOrIncludes('foo', ['foo', 'bar']) // true
- * isOrIncludes('foo', 'bar') // false
- * isOrIncludes('foo', ['bar', 'baz']) // false
- */
-export const isOrIncludes = <T, V extends T | T[]>(value: T, against: V) => {
-  if (Array.isArray(against)) {
-    return against.includes(value)
-  }
-  return value === (against as unknown as T)
-}
